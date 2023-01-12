23cba46f8de80207efe59782cba3b4c3a2e40204
PierBezuhov <pier.bezuhov.wm@gmail.com>
2023-01-12 14:48:24 +0200

    Complited react course

--- 26-react-router/src/App.jsx
+++ 26-react-router/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/Home'
 import Contacts from './components/Contacts'
 import About from './components/About'
 import NotFound from './components/NotFound'
+import Courses from './components/Courses'
+import SingleCourse from './components/SingleCourse'
 import './App.css'
 
 function App() {
@@ -13,9 +15,11 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="courses/:courseSlug" element={<SingleCourse />} />
             <Route path="about" element={<About />} />
             <Route path="contacts" element={<Contacts />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </div>
---
+++ 26-react-router/src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    title: 'Complete React Guide',
+    slug: 'react',
+    id: 257,
+  },
+  {
+    title: 'Complete JavaScrips Guide',
+    slug: 'javascript',
+    id: 537,
+  },
+  {
+    title: 'Complete Python Guide',
+    slug: 'python',
+    id: 133,
+  },
+  {
+    title: 'Complete Java Guide',
+    slug: 'java',
+    id: 845,
+  },
+]
+
+export default courses
